843fa264b1db430befe0cae332144b521e32b796
SIDAKGROVER <sidaksingh269@gmail.com>
2025-02-24 10:43:36 +0530

    python program

---
+++ 1.py
@@ -0,0 +1,12 @@
+n=int(input("enter a no."))
+sum=0
+order=len(str(n))
+copy_n=n
+while n>0:
+    digit=n%10
+    sum+=digit**order
+    n=n//10
+if sum==copy_n:
+    print("armstrong no")
+else:
+    print("not armstrong no.")
